ab4fd525b480109bd455c67c18d6ab38d0ed64d1
Sanchit Goyal <sanchitgoyal0911@gmail.com>
2026-06-28 18:45:47 +0530

    Create Stocks.cpp

---
+++ Stocks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int maxProfit = 0;
+        int n = prices.size();
+
+        for(int i = 0; i < n; i++) {
+            int profit = prices[i] - mini;
+
+            maxProfit = max(maxProfit, profit);
+
+            mini = min(mini, prices[i]);
+        }
+
+        return maxProfit;
+    }
+
+};
